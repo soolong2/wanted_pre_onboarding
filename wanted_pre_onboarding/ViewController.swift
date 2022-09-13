13690dd2a3e989024843ab05d1755cc19f9dd1d0
soo <gustn9360@naver.com>
2022-09-13 20:56:44 +0900

    원티드 프로젝트 시작

---
+++ wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by so on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
